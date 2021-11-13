6c308c25d6e87995a7b500f7806933fd03c35e0c
Charlie <achanont@gmail.com>
2021-11-13 22:06:50 +0700

    add polygon api

--- src/utils/getBalances.js
+++ src/utils/getBalances.js
@@ -61,6 +61,24 @@ export async function getOptimisticBalance(address) {
   return formatbalance(weibalance);
 }
 
+export async function getPolygonBalance(address) {
+  const { data } = await Axios({
+    method: "get",
+    url: "https://api.polygonscan.com/api",
+    params: {
+      module: "account",
+      action: "tokenbalance",
+      address: address,
+      contractaddress: "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619",
+      tag: "latest",
+      // eslint-disable-next-line no-undef
+      apikey: process.env.POLYGON_API_KEY,
+    },
+  });
+  const weibalance = data?.result || "0";
+  return formatbalance(weibalance);
+}
+
 // utils function
 function formatbalance(weibalance) {
   const etherbalance = web3.utils.fromWei(weibalance, "ether");
